2f178b059183d6541e6bbbc029df6931147431b0
Tony Dunsworth <tony.dunsworth@gmail.com>
2021-04-09 14:14:08 -0400

    Add files via upload
    New datasets and query to generate them.

--- analysis_qry.sql
+++ analysis_qry.sql
@@ -19,8 +19,18 @@ SELECT Response_Date,
         WHEN Agency_Type = 'FIRE' AND NOT (Problem LIKE ('%ALS%') OR Problem LIKE ('%BLS%') OR Problem IN ('CONSTRUCTION SITE INJURY','PSYCHIATRIC EMERGENCY VIOLENT','PUBLIC SERVICE EMS')) THEN 'FIRE'
 		ELSE 'DECC'
 	END AS [Agency],
-    COALESCE(NULLIF(MethodOfCallRcvd,''), 'Not Reported') MethodOfCallRcvd,
-    Fixed_Time_PhonePickUp,
+    CASE
+		WHEN Problem LIKE 'MUTUAL%' THEN 'MUTUAL AID'
+		WHEN MethodofCallRcvd IS NULL AND Problem IN ('TRAFFIC STOP', 'OCCUPIED VEHICLE CHECK', 'SUBJECT STOP', 'FLAG DOWN','OFF DUTY DETAIL','ASSIST CITIZEN DELAY','DISABLED MOTORIST','ON VIEW') THEN 'OFFICER'
+		WHEN MethodOfCallRcvd = '' THEN 'Not Reported'
+		WHEN MethodOfCallRcvd IS NULL AND Problem NOT IN ('TRAFFIC STOP', 'OCCUPIED VEHICLE CHECK', 'SUBJECT STOP', 'FLAG DOWN','OFF DUTY DETAIL','ASSIST CITIZEN DELAY','DISABLED MOTORIST','ON VIEW') THEN 'Not Reported'
+		ELSE MethodOfCallRcvd
+	END AS [Call Received],
+    CASE
+		WHEN Problem LIKE 'MUTUAL%' THEN ClockStartTime
+		WHEN Fixed_Time_PhonePickUp IS NULL THEN Time_PhonePickUp
+		ELSE Fixed_Time_PhonePickUp
+	END AS [PhonePickUp],
     Fixed_Time_CallEnteredQueue,
     Time_First_Unit_Assigned,
     Fixed_Time_CallTakingComplete,
@@ -28,11 +38,10 @@ SELECT Response_Date,
     DATEDIFF(SECOND, Fixed_Time_CallEnteredQueue, Time_First_Unit_Assigned) AS [T2Disp],
     DATEDIFF(SECOND, Fixed_Time_PhonePickUp, Fixed_Time_CallTakingComplete) AS [ProcTime]
 FROM dbo.Response_Master_Incident
-WHERE Response_Date BETWEEN '2019-01-01' AND '2021-01-01'
+WHERE Response_Date BETWEEN '2020-01-01' AND '2021-01-01'
 AND CallTaking_Performed_By IN (SELECT Emp_Name FROM Personnel WHERE Emp_ID BETWEEN '4000' AND '4100')
 AND (Time_First_Unit_Assigned != '' OR Time_First_Unit_Assigned IS NOT NULL)
 AND Fixed_Time_PhonePickUp IS NOT NULL
-AND Problem NOT LIKE '%MUTUAL%'
 AND (Call_Disposition NOT IN ('TEST1-TEST CALL', 'TST-Test Call', 'TEST-Test Call','DUPPD-Duplicate Police','DUPFD-Duplicate Call Fire','IS-SYSTEM BACK IN SERVICE','SYSTEM BACK IN SERVICE') OR Call_Disposition IS NULL)
 ORDER BY Response_Date
 ;
