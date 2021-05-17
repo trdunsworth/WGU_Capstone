c87041dee0183ce83773b6c94c8d3d5ca9d009b4
Tony Dunsworth <tony.dunsworth@gmail.com>
2021-05-16 22:11:41 -0400

    16 May
    Updated test and details.

--- q1_21.r
+++ q1_21.r
@@ -2,19 +2,25 @@ dfq1 <- read_csv("Q1_21.csv")
 dfq1 %>% drop_na()
 str(dfq1)
 spec(dfq1)
+names(dfq1)[14] <- "Call_Taker"
 
 dfq1$Agency <- as.factor(dfq1$Agency)
 dfq1$Call_Disposition <- as.factor(dfq1$Call_Disposition)
 dfq1$Master_Incident_Number <- as.factor(dfq1$Master_Incident_Number)
-dfq1$Month <- as.factor(dfq1$Month)
-dfq1$DOW <- as.factor(dfq1$DOW)
+dfq1$Month <- factor(dfq1$Month, levels = c("January", "February", "March"))
+dfq1$DOW <- factor(dfq1$DOW, levels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))
 dfq1$Shift <- as.factor(dfq1$Shift)
 dfq1$Problem <- as.factor(dfq1$Problem)
 dfq1$Call_Received <- as.factor(dfq1$Call_Received)
-dfq1$`Call Taker` <- as.factor(dfq1$`Call Taker`)
 dfq1$Dispatcher <- as.factor(dfq1$Dispatcher)
 dfq1$Call_Taker <- as.factor(dfq1$Call_Taker)
+dfq1$WkNo <- as.factor(dfq1$WeekNo)
+dfq1$PriNo <- as.factor(dfq1$Priority_Number)
+dfq1$Year <- as.character(dfq1$Year)
 
+q1c1 <- count(dfq1, Month, Agency, Shift)
+q1c2 <- count(dfq1, WeekNo, Shift, Agency)
+q1c3 <- count(dfq1, DOW, Shift, Agency)
 
 dfq1$QueueTime <- as.numeric(dfq1$Fixed_Time_CallEnteredQueue - dfq1$Phone_Pick_Up)
 dfq1$DispatchTime <-as.numeric(dfq1$Time_First_Unit_Assigned - dfq1$Fixed_Time_CallEnteredQueue)
@@ -37,7 +43,7 @@ Q1AgcyTbl<- table(Agency)
 Q1ProbTbl <- table(Problem)
 Q1PNTbl <- table(Priority_Number)
 Q1RcvTbl <- table(Call_Received)
-Q1CTTbl <- table(`Call Taker`)
+Q1CTTbl <- table(Call_Taker)
 Q1DSpTbl <- table(Dispatcher)
 Q1CDTbl <- table(Call_Disposition)
 
@@ -88,9 +94,46 @@ ggscatter(dfq1, x = "QueueTime", y = "DispatchTime",
           color = "DOW",
           palette = get_palette("jco", 7))
 
+p <- ggboxplot(dfq1, x = "DOW", y = "QueueTime",
+               color = "DOW", palette = "startrek",
+               add = "jitter")
+
+#  Add p-value
+p + stat_compare_means()
+# Change method
+p + stat_compare_means(method = "t.test")
+
+p2 <- ggboxplot(dfq1, x = "Shift", y = "QueueTime", color = "Shift", palette = "startrek", add = "jitter")
+p2 + stat_compare_means()
+
+p3 <- ggboxplot(dfq1, x = "Month", y = "QueueTime", color = "Month", palette = "jco", add = "jitter")
+p3 + stat_compare_means()
+monCom <- list(c("January", "February"), c("February", "March"), c("January", "March"))
+p3 + stat_compare_means(comparisons = monCom) + stat_compare_means(label.y = 3500)
+kruskal.test(QueueTime ~ Month, data = dfq1)
+kruskal_effsize(QueueTime ~ Month, data = dfq1)
+dunn_Q_Mon <- dunn_test(dfq1, QueueTime ~ Month, p.adjust.method = "bonferroni")
+
+p3a <- ggboxplot(dfq1, x = "Month", y = "DispatchTime", color = "Month", palette = "startrek", add = "jitter")
+p3a + stat_compare_means()
+p3a + stat_compare_means(comparisons = monCom) + stat_compare_means(label.y = 150000)
+
+p3b <- ggboxplot(dfq1, x = "Month", y = "ProcessTime", color = "Month", palette = "startrek", add = "jitter")
+p3b + stat_compare_means()
+p3b + stat_compare_means(comparisons = monCom) + stat_compare_means(label.y = 150000)
+kruskal.test(ProcessTime ~ Month, data = dfq1)
+dunn_proc_mon <- dunn_test(dfq1, DispatchTime ~ Month, p.adjust.method = "bonferroni")
+
+p3c <- ggboxplot(dfq1, x = "Month", y = "CallTime", color = "Month", palette = "startrek", add = "jitter")
+p3c + stat_compare_means()
+p3c + stat_compare_means(comparisons = monCom) + stat_compare_means(label.y = 15000)
+kruskal.test(CallTime ~ Month, data = dfq1)
+kruskal_effsize(CallTime ~ Month, data = dfq1)
+dunn_call_mon <- dunn_test(dfq1, CallTime ~ Month, p.adjust.method = "bonferroni")
+
 ggbarplot(Q1DowDf, x = "DOW", y = "Freq",
           fill = "DOW",
-          palette = "jco",            
+          palette = "startrek",            
           x.text.angle = 90,
           title = "Call Count by Day of Week",
           xlab = "Day of Week",
@@ -140,7 +183,7 @@ mean(CallTime)
 mean(CallTime, 0.1)
 mean(CallTime, 0.2)
 
-median(QueueTime ~ `Call Taker`)
+median(QueueTime ~ Call_Taker)
 median(DispatchTime ~ Dispatcher)
 median(DispatchTime ~ Dispatcher + Priority_Number)
 
@@ -148,6 +191,9 @@ median(QueueTime ~ Agency)
 median(DispatchTime ~ Agency)
 median(CallTime ~ Agency)
 median(ProcessTime ~ Agency)
+
+q1_mean_prob_disp <- compare_means(DispatchTime ~ Problem, data = dfq1)
+q1_mean_prob_disp2 <- compare_means(DispatchTime ~ Problem, data = dfq1, method = "kruskal.test")
 
 write.table(Q1ProbTbl, file = "q1_problem_count.txt", sep = ",", quote = FALSE, row.names = F)
 
@@ -157,10 +203,37 @@ ggplot(dfq1) + aes(x = DOW, fill = Shift) + geom_bar()
 
 median(QueueTime ~ Shift + DOW, data = dfq1)
 
-kruskal.test(QueueTime ~ `Call Taker`, data = dfq1)
-kruskal_effsize(QueueTime ~ `Call Taker`, data = dfq1)
-pairwise.wilcox.test(QueueTime, `Call Taker`, p.adjust.method = "BH", data = dfq1)
+kruskal.test(QueueTime ~ Call_Taker, data = dfq1)
+kruskal_effsize(QueueTime ~ Call_Taker, data = dfq1)
+pairwise.wilcox.test(QueueTime, Call_Taker, p.adjust.method = "BH", data = dfq1)
 dunn_test(dfq1, QueueTime ~ DOW, p.adjust.method = "bonferroni")
 dunn_Q_CT <- dunn_test(dfq1, QueueTime ~ Call_Taker, p.adjust.method = "bonferroni")
+
+ggerrorplot(dfq1, x = "Month", y = "QueueTime", 
+            desc_stat = "median_iqr", color = "black",
+            add = "violin", add.params = list(color = "darkgray")) + stat_compare_means(comparisons = monCom) + stat_compare_means(label.y = 3500)
+
+ggline(dfq1, x = "Month", y = "QueueTime", 
+       add = c("median_iqr", "jitter"),
+       color = "Shift", palette = "jco")
+
+ggline(dfq1, x = "Month", y = "QueueTime", 
+       add = c("median_iqr", "jitter"),
+       color = "DOW", palette = "jco")
+
+ggboxplot(dfq1, x = "DOW", y = "QueueTime", color = "DOW", 
+          add = "jitter", legend = "none", palette = "jco") +
+  rotate_x_text(angle = 45)+
+  geom_hline(yintercept = median(dfq1$QueueTime), linetype = 2)+ # Add horizontal line at base mean
+  stat_compare_means(method = "kruskal.test", label.y = 3500)+        # Add global annova p-value
+  stat_compare_means(label = "p.signif", method = "wilcox.test", ref.group = ".all.", hide.ns = TRUE)
+
+q1a <- ggboxplot(dfq1, x = "Shift", y = "QueueTime",
+               color = "Shift", palette = "jco",
+               add = "jitter",
+               facet.by = "Month", short.panel.labs = FALSE)
+# Use only p.format as label. Remove method name.
+q1a +  stat_compare_means(method = "kruskal.test", label.y = 3000) + 
+  stat_compare_means(label = "p.signif", method = "wilcox.test", ref.group = ".all.", hide.ns = TRUE)
 
 detach(dfq1)
